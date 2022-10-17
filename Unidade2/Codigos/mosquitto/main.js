ec6881d87e8d50dae9637021209e31348daa5c38
fabiorochaufsc <fabio.rocha@gmail.com>
2022-10-17 14:00:37 -0300

    ss

--- Unidade2/Codigos/mosquitto/main.js
+++ Unidade2/Codigos/mosquitto/main.js
@@ -15,11 +15,11 @@ client.on('message', function (topic, message) {
   // message is Buffer
   console.log('recebeu msg do topico:'+topic)
   console.log(message.toString())
-  client.end()
+
 })
 
 
-setTimeout(function(){
+setInterval(function(){
 
   client.publish('temperatura', '30.23')
 },3000);
